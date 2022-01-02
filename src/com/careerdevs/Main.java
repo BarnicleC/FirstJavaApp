5edf3a5b54f88468e8f4b8903c93e197a577ce89
BarnicleC <barnicle.carla@gmail.com>
2022-01-01 19:22:38 -0500

    Created the hello method  hello function outputs a greeting to the terminal with a given string "name"

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -5,6 +5,12 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello CareerDevs");
-        
+        hello("Jazlyn");
+        hello(  "Gabriel");
+    }
+
+    // hello function outputs a greeting to the terminal with a given string "name"
+    public static void hello(String name) {
+        System.out.println("hello "+name);
     }
 }
